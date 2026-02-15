a287f2fa4f251c4168c21736cdf7f1a128c4fa24
brycen-newell <brycen.newell@proton.me>
2026-02-14 21:13:35 -0600

    Create variables.tf

---
+++ terraform/dev/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in."
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "ssh_key_name" {
+  description = "The name of the EC2 Key Pair for SSH"
+  type        = string
+  default     = "deployer-key"
+}
+
+variable "ssh_public_key" {
+  description = "The public key to be used for SSH access."
+  type        = string
+  
+
+}
