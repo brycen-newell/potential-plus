897aaf4aea5e7170b727ff2c4bfb95c839401644
brycen-newell <brycen.newell@proton.me>
2026-02-16 18:28:22 -0600

    Update main.tf
    missing close quote added at line 106

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -103,5 +103,5 @@ resource "aws_instance" "web_server_dev" {
 
 resource "local_file" "ansible_inventory_dev" {
   content  = "[webservers]\n${aws_instance.web_server_dev.public_ip} ansible_user=ec2-user"
-  filename = "${path.root}/../../ansible/inventory/dev.ini 
+  filename = "${path.root}/../../ansible/inventory/dev.ini"
 }
