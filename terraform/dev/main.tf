43690d906d18d489e425c57573fc6c1cdcf98133
brycen-newell <brycen.newell@proton.me>
2026-02-21 19:02:59 -0600

    Update main.tf

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -128,7 +128,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${var.runner)ip}/32"]
+    cidr_blocks = ["${var.runner_ip}/32"]
   }
 
   egress {
