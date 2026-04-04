a1393106ed485d357d5fb751e5d16e50c97e08b6
Brycen Newell <brycen.newell@proton.me>
2026-04-04 02:31:27 -0500

    updated alb with second region (and comma)

--- terraform/dev/load_balancer.tf
+++ terraform/dev/load_balancer.tf
@@ -29,7 +29,7 @@ resource "aws_lb" "dev_alb" {
   load_balancer_type  = "application"
   security_groups     = [aws_security_group.lb_sg.id]
   subnets             = [
-      aws_subnet.dev_public_subnet.id 
+      aws_subnet.dev_public_subnet.id, 
       aws_subnet.dev_public_subnet_2.id 
     ]
   tags = {
